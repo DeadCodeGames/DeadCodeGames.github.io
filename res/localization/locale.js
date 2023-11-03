602a7101fa0947ae8bcbcd06c28e1092e8d09e88
Richard Kužela <richard@kanshen.click>
2023-11-03 18:57:31 +0100

    Lokalizácia (#3/#4)
    Stránka bola lokalizovaná do nasledujúcich jazykov:
    - Slovenčina - Slovak
    - Japončina - 日本語
    - Filipínčina - Filipino

---
+++ res/localization/locale.js
@@ -0,0 +1,27 @@
+function loadLocalization() {
+    const userLanguages = navigator.languages;
+    for (const language of userLanguages) {
+        const languageCode = language.split('-')[0];
+
+        if (languageCode === 'en') {
+            return;
+        };
+
+        fetch(`/res/localization/${languageCode}.json`).then((response) => response.json()).then((translations) => {
+                const translate = document.querySelectorAll('[string]');
+                translate.forEach((element) => {
+                    const string = element.getAttribute('string');
+                    if (translations[string]) {
+                        element.textContent = translations[string];
+                    }
+                });
+                document.querySelector("html").setAttribute("lang", languageCode);
+            })
+            .catch((error) => {
+                console.error('Error loading localization:', error);
+            });
+        return;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadLocalization);
